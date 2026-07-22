a8cab724ba10940bd338f8a941c6991e3b354a0c
Harsh Sahare <harshsahare1212@gmail.com>
2026-07-22 18:16:39 +0530

    basic setup

---
+++ app/lib/fonts.ts
@@ -0,0 +1,13 @@
+import { DM_Sans , Dancing_Script } from "next/font/google";
+
+export const dmSans = DM_Sans({
+  subsets: ["latin"],
+  style: ["normal"],
+  variable: "--font-dm-sans",
+});
+
+export const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+  style: ["normal"],
+  variable: '--font-dancing-script',
+});
