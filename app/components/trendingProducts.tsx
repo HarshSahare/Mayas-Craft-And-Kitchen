9fc18e43efda7e346355674e367c930ff0ae0551
Harsh Sahare <harshsahare1212@gmail.com>
2026-07-24 18:43:59 +0530

    added space between footer and trending page

--- app/components/trendingProducts.tsx
+++ app/components/trendingProducts.tsx
@@ -3,8 +3,8 @@ import ProductCard from "./productCard";
 
 function TrendingProducts() {
   return (
-    <div>
-      <div className="text-[16px] font-black font-dm text-[#2B2B2B] my-4">
+    <div className="my-4">
+      <div className="text-[16px] font-black font-dm text-[#2B2B2B] ">
         Popular Today
       </div>
       <div className="grid grid-cols-2 gap-2">
