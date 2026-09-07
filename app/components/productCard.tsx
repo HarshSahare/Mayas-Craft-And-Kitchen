3baeaf2db697ec572117ec178c78ad931a229200
Harsh Sahare <harshsahare1212@gmail.com>
2026-09-08 00:28:21 +0530

    increased the add the cart button in product cards

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -40,8 +40,8 @@ function ProductCard({
         />
         <Image
           src="/svg/product_card_add_cart_icon.svg"
-          width={30}
-          height={30}
+          width={40}
+          height={40}
           className="absolute bottom-0 aspect-square right-0"
           alt="add to cart button"
           onClick={(e) => {
