4464040ed3bdaecd881ee31dc831df38e336d457
Harsh Sahare <harshsahare1212@gmail.com>
2026-09-09 02:25:03 +0530

    increased the underline of modak in modak page title

--- app/modak/title.tsx
+++ app/modak/title.tsx
@@ -9,14 +9,14 @@ function Title() {
       </div>
       <div className="relative w-fit">
         <span className="text-[24px] font-extrabold">Modak</span>
-        <div className="absolute bottom-[-5] left-0 w-[70%] h-2">
+        <div className="absolute bottom-[-5] left-0 w-[110%] h-2">
           <Image
             fill
             src={"/svg/kitchenHeroSectionUnderline.svg"}
             alt="decoration"
           />
         </div>
-        <div className="absolute bottom-[-12] left-0 w-[50%] h-2">
+        <div className="absolute bottom-[-12] left-0 w-[80%] h-2">
           <Image
             fill
             src={"/svg/kitchenHeroSectionUnderline.svg"}
